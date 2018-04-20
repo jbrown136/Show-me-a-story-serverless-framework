d94bd47b01f0bdd4646323c107d56d05a0ddd044
tehbri <brian.mcstay@gmail.com>
2018-04-20 08:59:09 +0100

    updated serverless yml and handler function with custom image search

--- handler.js
+++ handler.js
@@ -1,4 +1,7 @@
 "use strict";
+const fetch = require("node-fetch");
+const { firebaseKeys, imageSearch } =
+  require("process.env") || require("./config");
 
 module.exports.endIntent = (event, context, callback) => {
   const response = {
@@ -32,28 +35,28 @@ module.exports.evaluateInput = (event, context, callback) => {
   //   }
   // }
 
-  const result = Object.entries(slots).reduce((acc, curr) => {
+  const currentSlot = Object.entries(slots).reduce((acc, curr) => {
     if (curr[1] === event.inputTranscript) return curr[0];
     return acc;
   }, "");
 
-  console.log(result);
-  switch (result) {
-    case "location":
-      console.log(event.inputTranscript);
-      break;
-    case "weather":
-      console.log(event.inputTranscript);
-      break;
-    case "mainCharacter":
-      console.log(event.inputTranscript);
-      break;
-    case "event":
-      console.log(event.inputTranscript);
-      break;
-  }
+  fetchAndSendImage(currentSlot, event.inputTranscript);
   callback(null, response);
 };
+
+function fetchAndSendImage(slot, text) {
+  return fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${imageSearch.apiKey}&cx=${
+      imageSearch.customSearchURL
+    }&q=${text}&num=1&imgSize=xlarge&searchType=image&safe=high&rights=cc_publicdomain`
+  )
+    .then(res => res.json())
+    .then(imgObj =>
+      console.log(JSON.stringify({ text, imageUrl: imgObj.items[0].link }))
+    )
+    .catch(console.log);
+}
+
 // {
 //   "currentIntent": {
 //     "name": "intent-name",
